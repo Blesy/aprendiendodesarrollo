f996728d58439c41559c731294392ca2dd52884e
Blesy <blesyangulo@gmail.com>
2023-12-13 15:16:09 -0700

    Add React JSX support and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import vercel from '@astrojs/vercel/serverless';
-
 import sitemap from '@astrojs/sitemap';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.aprendiendodesarrollo.com',
-	integrations: [mdx(), sitemap()],
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true,
-    },
-  }),
+  site: 'https://www.aprendiendodesarrollo.com',
+  integrations: [mdx(), sitemap(), react()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
