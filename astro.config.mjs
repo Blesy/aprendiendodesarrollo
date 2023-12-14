c4e0d995202ba8616a0aeb4216df744175bfc04f
Blesy <blesyangulo@gmail.com>
2023-12-14 01:14:01 -0700

    Update dependencies and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.aprendiendodesarrollo.com',
-  integrations: [mdx(), sitemap(), react()],
-  output: 'server',
+  integrations: [tailwind(), mdx(), sitemap(), react()],
+  output: 'static',
   adapter: vercel({
     webAnalytics: {
       enabled: true
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Leversi Aprendiendo Desarrollo';
+export const SITE_TITLE = 'Blesy Aprendiendo Desarrollo';
 export const SITE_DESCRIPTION = 'Bienvenido a mi blog, aquí encontrarás recursos en español de varios temas, desde libros a recursos en línea sobre lenguajes, conceptos, backend, frontend, videojuegos, etc.';
